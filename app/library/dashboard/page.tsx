f47e3b4ad5c3b263b1d956faa8ad3b3d3a9e05fc
otavio <otaviotemoteo@gmail.com>
2025-10-03 19:42:59 -0300

    fix: changed library routes

---
+++ app/library/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import Dashboard from "../../../components/library/dashboard/dashboard";
+
+const DashboardPage: React.FC = () => {
+  return <Dashboard />;
+};
+
+export default DashboardPage;
