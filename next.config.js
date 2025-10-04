3df96b52f87bbe5a7c6071a2cc12d3805bf7b38a
rafael rufino <rafaelceschirufino@gmail.com>
2025-10-03 22:36:15 -0300

    fix: delete next experimental config

--- next.config.js
+++ next.config.js
@@ -8,10 +8,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    appDir: true, // Habilita o app directory para Next.js 13+
-  },
+  }
 };
 
 export default nextConfig;
