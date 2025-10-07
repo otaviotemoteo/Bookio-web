b9049ee78b54d53c0c82e1406a2599ffc6c9a937
otavio <otaviotemoteo@gmail.com>
2025-10-07 18:59:36 -0300

    feat: loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,10 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
-  return <p>Carregando...</p>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <Loader2 className="w-12 h-12 text-blue-500 animate-spin mb-4" />
+      <p className="text-gray-700 text-lg">Carregando...</p>
+    </div>
+  );
 }
