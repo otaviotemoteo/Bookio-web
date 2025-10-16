614d2a70c78b0defea6de133316e35048661eea3
otavio <otaviotemoteo@gmail.com>
2025-10-15 21:23:22 -0300

    chore: add library create-reader types

---
+++ types/library/reader.ts
@@ -0,0 +1,45 @@
+export interface Address {
+  cep: string;
+  street: string;
+  neighborhood: string;
+  city: string;
+  number: string;
+}
+
+export interface Reader {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  libraryId: string;
+  address: Address;
+  picture?: string;
+  createdAt: string;
+  updatedAt: string;
+  activeLoans?: number;
+  totalLoans?: number;
+  pendingFines?: number;
+}
+
+export interface CreateReaderData {
+  name: string;
+  email: string;
+  libraryId: string;
+  cpf: string;
+  address: Address;
+}
+
+export interface UpdateReaderData {
+  name?: string;
+  email?: string;
+  cpf?: string;
+  address?: Address;
+}
+
+export interface ReaderFormData extends CreateReaderData {
+  picture?: File;
+}
+
+export interface UpdateReaderFormData extends UpdateReaderData {
+  picture?: File;
+}
