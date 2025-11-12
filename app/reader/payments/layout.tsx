9016f8e86d33272356b18063ae1d8ce387d540d4
otavio <otaviotemoteo@gmail.com>
2025-11-12 15:40:10 -0300

    fix: add sidebar on pages

--- app/reader/payments/layout.tsx
+++ app/reader/payments/layout.tsx
@@ -1,7 +1,21 @@
-export default function PaymentsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <>{children}</>;
+"use client";
+
+import React, { ReactNode } from "react";
+import Sidebar from "../../../components/reader/layout/sidebar";
+
+interface LayoutProps {
+  children: ReactNode;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
